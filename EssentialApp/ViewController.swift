ca0d0808c8a9d50b0a3462ea91e0c1619bbeb481
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-04-13 05:24:27 +0800

    Initial Commit

---
+++ EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Kai Chi Tsao on 2023/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
