98a2b27be075c9fef4f11d8439af621556218d2b
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-03 06:47:12 +0800

    Rename and move ResourceErrorView and ViewModel to Shared module

--- EssentialApp/WeakRefVirtualProxy.swift
+++ EssentialApp/WeakRefVirtualProxy.swift
@@ -10,14 +10,14 @@ import EssentialFeed_KaiLearning
 
 final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
-
+    
     init(_ object: T) {
         self.object = object
     }
 }
 
-extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
-    func display(_ viewModel: FeedErrorViewModel) {
+extension WeakRefVirtualProxy: ResourceErrorView where T: ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel) {
         object?.display(viewModel)
     }
 }
