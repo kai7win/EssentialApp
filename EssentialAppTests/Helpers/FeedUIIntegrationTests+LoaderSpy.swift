f78b0cd6d47ce2ec42a28ec12c7b9c09f83e74fd
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-22 16:43:01 +0800

    Prevent loading resources again until a running request completes

--- EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -38,6 +38,8 @@ extension FeedUIIntegrationTests{
             feedRequests[index].send(Paginated(items: feed, loadMorePublisher: { [weak self] in
                 self?.loadMorePublisher() ?? Empty().eraseToAnyPublisher()
             }))
+            
+            feedRequests[index].send(completion: .finished)
         }
         
         // MARK: - LoadMoreFeedLoader
