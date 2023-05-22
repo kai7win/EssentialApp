5147b490f03e9da9378f19ed5353a17447710334
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-22 15:17:46 +0800

    Cache page results

--- EssentialApp/CombineHelpers.swift
+++ EssentialApp/CombineHelpers.swift
@@ -29,7 +29,7 @@ public extension Paginated {
     
     var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
         guard let loadMore = loadMore else { return nil }
-
+        
         return {
             Deferred {
                 Future(loadMore)
@@ -41,10 +41,10 @@ public extension Paginated {
 
 public extension HTTPClient {
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
-
+    
     func getPublisher(url: URL) -> Publisher {
         var task: HTTPClientTask?
-
+        
         return Deferred {
             Future { completion in
                 task = self.get(from: url, completion: completion)
@@ -57,10 +57,10 @@ public extension HTTPClient {
 
 public extension FeedImageDataLoader {
     typealias Publisher = AnyPublisher<Data, Error>
-
+    
     func loadImageDataPublisher(from url: URL) -> Publisher {
         var task: FeedImageDataLoaderTask?
-
+        
         return Deferred {
             Future { completion in
                 task = self.loadImageData(from: url, completion: completion)
@@ -87,7 +87,7 @@ private extension FeedImageDataCache {
 
 public extension LocalFeedLoader {
     typealias Publisher = AnyPublisher<[FeedImage], Error>
-
+    
     func loadPublisher() -> Publisher {
         Deferred {
             Future(self.load)
@@ -102,8 +102,12 @@ extension Publisher {
     }
 }
 
-extension Publisher where Output == [FeedImage] {
-    func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
+extension Publisher {
+    func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> where Output == [FeedImage] {
+        handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
+    }
+    
+    func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> where Output == Paginated<FeedImage> {
         handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
     }
 }
@@ -111,6 +115,10 @@ extension Publisher where Output == [FeedImage] {
 private extension FeedCache {
     func saveIgnoringResult(_ feed: [FeedImage]) {
         save(feed) { _ in }
+    }
+    
+    func saveIgnoringResult(_ page: Paginated<FeedImage>) {
+        saveIgnoringResult(page.items)
     }
 }
 
@@ -124,19 +132,19 @@ extension DispatchQueue {
     static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
         ImmediateWhenOnMainQueueScheduler.shared
     }
-
+    
     struct ImmediateWhenOnMainQueueScheduler: Scheduler {
         typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
         typealias SchedulerOptions = DispatchQueue.SchedulerOptions
-
+        
         var now: SchedulerTimeType {
             DispatchQueue.main.now
         }
-
+        
         var minimumTolerance: SchedulerTimeType.Stride {
             DispatchQueue.main.minimumTolerance
         }
-
+        
         static let shared = Self()
         
         private static let key = DispatchSpecificKey<UInt8>()
@@ -156,11 +164,11 @@ extension DispatchQueue {
             }
             action()
         }
-
+        
         func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) {
             DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
         }
-
+        
         func schedule(after date: SchedulerTimeType, interval: SchedulerTimeType.Stride, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable {
             DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, options: options, action)
         }
