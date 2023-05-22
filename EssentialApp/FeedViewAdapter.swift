79943a57b6856caa5d8ca683db24b309df2afcae
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-22 13:00:27 +0800

    Notifies selection handler on image selection

--- EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/FeedViewAdapter.swift
@@ -12,14 +12,16 @@ import EssentialFeediOS
 final class FeedViewAdapter: ResourceView {
     private weak var controller: ListViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private let selection: (FeedImage) -> Void
     
     private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>
-
-    init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
+    
+    init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher, selection: @escaping (FeedImage) -> Void) {
         self.controller = controller
         self.imageLoader = imageLoader
+        self.selection = selection
     }
-
+    
     func display(_ viewModel: FeedViewModel) {
         controller?.display(viewModel.feed.map { model in
             let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
@@ -28,7 +30,10 @@ final class FeedViewAdapter: ResourceView {
             
             let view = FeedImageCellController(
                 viewModel: FeedImagePresenter.map(model),
-                delegate: adapter)
+                delegate: adapter,
+                selection: { [selection] in
+                    selection(model)
+                })
             
             adapter.presenter = LoadResourcePresenter(
                 resourceView: WeakRefVirtualProxy(view),
@@ -43,7 +48,7 @@ final class FeedViewAdapter: ResourceView {
 
 extension UIImage {
     struct InvalidImageData: Error {}
-
+    
     static func tryMake(data: Data) throws -> UIImage {
         guard let image = UIImage(data: data) else {
             throw InvalidImageData()
